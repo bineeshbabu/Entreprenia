a91789701144d210f59746a674546e3abdf7ba49
MySachin2 <sachingiridhar@gmail.com>
2016-09-04 17:17:59 +0530

    Deleted old fragments and Added partner

--- app/src/main/java/com/phacsin/entreprenia/main/partners/Partners.java
+++ app/src/main/java/com/phacsin/entreprenia/main/partners/Partners.java
@@ -2,16 +2,25 @@ package com.phacsin.entreprenia.main.partners;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+
 import com.phacsin.entreprenia.R;
 
 /**
  * Created by Bineesh P Babu on 19-08-2016.
  */
 public class Partners extends AppCompatActivity {
+    Toolbar toolbar;
+    RecyclerView recyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_partners);
+        toolbar =(Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Partners");
+        toolbar.setNavigationIcon(R.drawable.abc_ic_ab_back_mtrl_am_alpha);
+        setSupportActionBar(toolbar);
     }
 
 }
