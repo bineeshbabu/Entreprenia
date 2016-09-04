ff39210761a0fb0428f7fe4c6925e0bbb9689600
MySachin2 <sachingiridhar@gmail.com>
2016-09-04 18:08:28 +0530

    Completed Partner

---
+++ app/src/main/java/com/phacsin/entreprenia/PartnerAdapter.java
@@ -0,0 +1,78 @@
+package com.phacsin.entreprenia;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.phacsin.entreprenia.customfonts.MyTextView;
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
+
+import java.util.List;
+
+import me.drakeet.materialdialog.MaterialDialog;
+
+/**
+ * Created by GD on 9/4/2016.
+ */
+public class PartnerAdapter extends RecyclerView.Adapter<PartnerAdapter.MyViewHolder> {
+
+    private List<String> partnerList;
+    Context context;
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        ImageView imageView;
+        MyTextView details;
+
+        public MyViewHolder(View view) {
+            super(view);
+            imageView = (ImageView) view.findViewById(R.id.partner_image);
+        }
+    }
+
+
+    public PartnerAdapter(List<String> partnerList,Context context) {
+        this.partnerList = partnerList;
+        this.context = context;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.partner_list_row, parent, false);
+
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(final MyViewHolder holder, int position) {
+        String URL = partnerList.get(position);
+        Target target = new Target() {
+            @Override
+            public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
+                holder.imageView.setImageBitmap(bitmap);
+            }
+
+            @Override
+            public void onBitmapFailed(Drawable errorDrawable) {
+
+            }
+
+            @Override
+            public void onPrepareLoad(Drawable placeHolderDrawable) {
+
+            }
+        };
+        Picasso.with(context).load(URL).resize(1280,720).onlyScaleDown().into(target);
+    }
+
+    @Override
+    public int getItemCount() {
+        return partnerList.size();
+    }
+}
