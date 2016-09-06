729a8a5de479f9051131df57984efb179b29b38f
bineeshbabupattat <bineeshbabupattat@gmail.com>
2016-09-06 23:45:30 +0530

    entreprenia details

--- app/src/main/java/com/phacsin/entreprenia/main/Entreprenia.java
+++ app/src/main/java/com/phacsin/entreprenia/main/Entreprenia.java
@@ -1,7 +1,11 @@
 package com.phacsin.entreprenia.main;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 import com.phacsin.entreprenia.R;
 
@@ -14,5 +18,13 @@ public class Entreprenia extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_entreprenia);
+        RelativeLayout r = (RelativeLayout)findViewById(R.id.lay_details);
+        r.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.entreprenia.org"));
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/phacsin/entreprenia/registration/LoginActivity.java
+++ app/src/main/java/com/phacsin/entreprenia/registration/LoginActivity.java
@@ -199,7 +199,6 @@ public class LoginActivity extends AppCompatActivity {
         } else {
             _passwordText.setError(null);
         }
-
         return valid;
     }
 }
